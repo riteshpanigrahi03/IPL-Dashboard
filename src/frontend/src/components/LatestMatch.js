02a9aaf5e472c863095417fbd840df2cc1aae266
riteshpanigrahi03 <panigrahiritesh03@gmail.com>
2021-05-30 16:10:09 +0530

    completed teampage

--- src/frontend/src/components/LatestMatch.js
+++ src/frontend/src/components/LatestMatch.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export const LatestMatch=({teamName,match}) => {
     if(!match) return null;
     // console.log(latestMatch);
     const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
+    const otherTeamRoute = `/teams/${otherTeam}`
 
     return (
         <div>
             <h1>Latest Match</h1>
             
-            <h1>vs {otherTeam}</h1>
+            <h1>vs
+            <Link to= {otherTeamRoute}> {otherTeam}</Link>
+            </h1>
             <h2>{match.date}</h2>
             <h3>{match.venue}</h3>
             <h3>{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,13 @@
+import {React, useEffect,useState} from 'react';
+import {useParams } from 'react-router-dom'
+import { LatestMatch } from '../components/LatestMatch';
+import { OtherMatches } from '../components/OtherMatches';
+
+export const MatchPage = (props) => {
+    
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+        </div>
+    );
+}
