80188854884f2fa20ab9598cb504380f17acf9ba
riteshpanigrahi03 <panigrahiritesh03@gmail.com>
2021-05-30 16:47:28 +0530

    added match page

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -5,6 +5,21 @@ import { OtherMatches } from '../components/OtherMatches';
 
 export const MatchPage = (props) => {
     
+    const [matches,setMatches] = useState([]);
+    const {teamName,year} = useParams();
+    useEffect(()=>{
+
+        const fetchMatches = async () =>{
+            const response = await fetch(`http://localhost:8095/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            console.log(data);
+            setMatches(data);
+            //console.log(team);
+
+        }
+        fetchMatches();
+    },[]);
+
     return (
         <div className="MatchPage">
             <h1>Match Page</h1>
