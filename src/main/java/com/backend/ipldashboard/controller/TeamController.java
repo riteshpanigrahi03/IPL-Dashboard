eaf7929bd44e3980c6d6b0dcada380c938b8ff18
riteshpanigrahi03 <panigrahiritesh03@gmail.com>
2021-05-30 16:30:21 +0530

    added backend match api

--- src/main/java/com/backend/ipldashboard/controller/TeamController.java
+++ src/main/java/com/backend/ipldashboard/controller/TeamController.java
@@ -1,16 +1,16 @@
 package com.backend.ipldashboard.controller;
 
+import com.backend.ipldashboard.model.Match;
 import com.backend.ipldashboard.model.Team;
 import com.backend.ipldashboard.repo.MatchRepo;
 import com.backend.ipldashboard.repo.TeamRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Pageable;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -27,5 +27,16 @@ public class TeamController {
         team.setMatches(matchRepo.findMatchedsByTeam(teamName,4));
 
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        return this.matchRepo.getMatchesByTeamBetweenDates(
+                teamName,
+                startDate,
+                endDate
+        );
     }
 }
--- src/main/java/com/backend/ipldashboard/repo/MatchRepo.java
+++ src/main/java/com/backend/ipldashboard/repo/MatchRepo.java
@@ -3,8 +3,11 @@ package com.backend.ipldashboard.repo;
 import com.backend.ipldashboard.model.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface MatchRepo extends CrudRepository<Match,Long> {
@@ -13,4 +16,11 @@ public interface MatchRepo extends CrudRepository<Match,Long> {
     default List<Match> findMatchedsByTeam(String teamName,int count){
         return  getByTeam1OrTeam2OrderByDateDesc(teamName,teamName, PageRequest.of(0,count));
     }
+
+    @Query("select m from Match m where (m.team1 = :teamName or m.team2 = :teamName) and m.date between :dateStart and :dateEnd order by date desc")
+    List<Match> getMatchesByTeamBetweenDates(
+            @Param("teamName") String teamName,
+            @Param("dateStart") LocalDate dateStart,
+            @Param("dateEnd") LocalDate dateEnd
+    );
 }
